b5a8660e61d8fd893f54f082b7453fccf42b60ef
John Haus <john.haus@outlook.com>
2025-10-24 13:30:18 -0400

    create a global style component and move global styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,23 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import Home from './Home.jsx'
 import About from './About.jsx'
+import { createGlobalStyle } from 'styled-components'
 
 const root = document.getElementById("root");
 
+const GlobalStyles = createGlobalStyle`
+  #root {
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 2rem;
+    text-align: center;
+  }
+
+`
+
 ReactDOM.createRoot(root).render(
   <StrictMode>
+    <GlobalStyles />
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
