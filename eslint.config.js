aee446c564ec986f359ecd77daf389cec0c92adf
John Haus <john.haus@outlook.com>
2025-10-30 16:18:40 -0400

    add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,19 +2,25 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettierPlugin from 'eslint-plugin-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'build']),
+
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      prettier: prettierPlugin,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
@@ -22,8 +28,12 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+
     rules: {
+      'prettier/prettier': 'error',
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'eol-last': ['error', 'always'],
+      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 1 }],
     },
   },
 ])
