1571d055f401ee976547be58020067286167b981
John Haus <john.haus@outlook.com>
2025-10-31 13:41:37 -0400

    set up storybook

---
+++ .storybook/preview.jsx
@@ -0,0 +1,29 @@
+/** @type { import('@storybook/react-vite').Preview } */
+import { MemoryRouter } from 'react-router';
+
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo',
+    },
+  },
+  decorators: [
+    (Story) => (
+      <MemoryRouter>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+};
+
+export default preview;
---
+++ src/shared/Navbar/Header.stories.jsx
@@ -0,0 +1,14 @@
+import { fn } from 'storybook/test';
+import Header from './Header';
+
+export default {
+  title: 'Navbar/Header',
+  component: Header,
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+export const Default = {
+};
