b31e77b18dc49035a45709c7bde77179a9783140
John Haus <john.haus@outlook.com>
2025-11-04 14:01:36 -0500

    remove unused items

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import styled, { keyframes } from 'styled-components';
 import Header from './shared/navbar/header';
 
