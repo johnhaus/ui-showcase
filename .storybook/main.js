e6b9dd8b2924b8419dd3a0d25b06a4273e915178
John Haus <john.haus@outlook.com>
2025-12-03 22:34:28 -0500

    add pixelmatch

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ const config = {
     '@storybook/addon-onboarding',
     '@storybook/addon-a11y',
     '@storybook/addon-vitest',
+    '@storybook/test-runner',
   ],
   framework: {
     name: '@storybook/react-vite',
