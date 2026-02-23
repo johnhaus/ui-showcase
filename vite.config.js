c46390c85dfd8a4088dccad649ef1373f30f1a72
John Haus <john.haus@outlook.com>
2026-02-23 10:17:54 -0500

    update project name and regenerate lock file

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/demo-website/',
+  base: '/ui-showcase/',
   plugins: [react()],
   test: {
     projects: [
